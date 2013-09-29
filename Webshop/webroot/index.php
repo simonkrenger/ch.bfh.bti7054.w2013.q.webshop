4214859a008dcf823df454e57c827218f28fb84e
Simon Krenger <simon@krenger.ch>
2013-09-29 16:23:05 +0200

    Hopefully the last commit to test autodeploy

--- Webshop/webroot/index.php
+++ Webshop/webroot/index.php
@@ -1,3 +1,9 @@
+<!DOCTYPE unspecified PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head><title>PlanetShop</title></head>
+<body>
 <?php
-echo "Hello World.";
+echo "Hello World!";
 ?>
+</body>
+</html>
