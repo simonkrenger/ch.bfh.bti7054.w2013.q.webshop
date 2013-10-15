11445215d978d52b5ee9ca3762f615ca1d935360
Simon Krenger <simon@krenger.ch>
2013-10-15 23:49:24 +0200

    Formatted header.php and sidebar.php, added menu links CSS

--- Webshop/webroot/header.php
+++ Webshop/webroot/header.php
@@ -11,35 +11,55 @@
 		<div id="navigation">
 			<div id="logo">
 				<div id="companyname">
-					<a href="/"><img src="images/logo-white.png" alt="Logo" />
-						PlanetShop </a>
+					<a href="/"><img src="images/logo-white.png" alt="Logo" /></a>
 				</div>
 			</div>
 
 			<!-- 			<div id="search"></div> -->
 			<?php
 			$menu = array (
-					"home" => array ("name" => "Home", "href" => "/index.php"),
-					"predefined" => array ("name" => "Predefined", "href" => "/default.php"),
-					"onsale" => array ("name" => "On Sale", "href" => "/default.php"),
-					"custom" => array ("name" => "Custom", "href" => "/default.php"),
-					"satellites" => array ("name" => "Satellites", "href" => "/default.php"),
-					"accessories" => array ("name" => "Accessories", "href" => "/default.php"),
-					"shoppingcart" => array ("name" => "Shopping Cart", "href" => "/default.php"),
-					"login" => array ("name" => "Login", "href" => "/default.php")
+					"home" => array (
+							"name" => "Home",
+							"href" => "/index.php" 
+					),
+					"predefined" => array (
+							"name" => "Predefined",
+							"href" => "/default.php" 
+					),
+					"onsale" => array (
+							"name" => "On Sale",
+							"href" => "/default.php" 
+					),
+					"custom" => array (
+							"name" => "Custom",
+							"href" => "/default.php" 
+					),
+					"satellites" => array (
+							"name" => "Satellites",
+							"href" => "/default.php" 
+					),
+					"accessories" => array (
+							"name" => "Accessories",
+							"href" => "/default.php" 
+					),
+					"shoppingcart" => array (
+							"name" => "Shopping Cart",
+							"href" => "/default.php" 
+					),
+					"login" => array (
+							"name" => "Login",
+							"href" => "/default.php" 
+					) 
 			);
-
 			
 			?>
 			<div id="menu">
-<!-- 				<div class="menuentry" id="home"><a href="/index.html">Home</a></div> -->
-				<?php 	
-						
-			foreach ($menu as $id => $link) {
-				echo "<div class=\"menuentry\" id=\"$id\"> <a href=\"$link[href]\">$link[name]</a> </div>";
-
-			}?>
+				<!-- 				<div class="menuentry" id="home"><a href="/index.html">Home</a></div> -->
+				<?php
+				
+				foreach ( $menu as $id => $link ) {
+					echo "<div class=\"menuentry\" id=\"$id\"> <a href=\"$link[href]\">$link[name]</a> </div>";
+				}
+				?>
 			</div>
-
-
 		</div>
