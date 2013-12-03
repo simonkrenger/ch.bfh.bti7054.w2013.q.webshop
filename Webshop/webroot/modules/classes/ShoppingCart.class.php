505bbce3eab75ddb7c8285bc21fa0476c03aaaa4
corrf1 <corrf1@bfh.ch>
2013-12-03 21:30:48 +0100

    - updated ToDo - changed shoppingCart functions

--- Webshop/webroot/modules/classes/ShoppingCart.class.php
+++ Webshop/webroot/modules/classes/ShoppingCart.class.php
@@ -24,13 +24,13 @@ class ShoppingCart {
 		
 		echo "<table border=\"1\">";
 		echo "<tr><th>Article</th><th>Items</th></tr>";
-		foreach ( $this->items as $art => $num )
+		foreach ( $this->items as $art => $num ) ;
 			
 		echo "<tr><td>$art</td><td>$num</td></tr>";
 		echo "</table>";
 		
-		$prod_info = getProductInformation($art);
-		$prod_info->name;
+		$prod_info = $this->getProductInformation($art);
+		echo $prod_info->name;
 		
 		
 		//echo "<div class=\"cartField\"> <label for =\"name\">ProductName</label><input type=\"" . $entry [2] . "\" name=\"" . $entry[0] . "\" size=\"" . $entry[3] . "\" maxlength=\"" . $entry[4] . "\" id=\"" . $entry[0] . "\" placeholder =\"" . $entry[0] . "\"></div>";
@@ -39,8 +39,9 @@ class ShoppingCart {
 	}
 	
 	private function getProductInformation($id){
+		global $shopdb;
 		$product_id = $shopdb->escape($id);
-		$query = sprintf ( "SELECT product_id, name, product_picture, description, price, inventory_quantity FROM product WHERE product_id=%s", $product_id );
+		$query = sprintf ( "SELECT product_id, name, product_picture, description, price, inventory_quantity FROM product WHERE product_id=%s", $id );
 		return $shopdb->get_row( $query );
 	}
 	
