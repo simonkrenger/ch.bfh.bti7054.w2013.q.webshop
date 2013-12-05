e94b9583dc470b07f64ab7afba01e4e188821f40
Simon Krenger <simon@krenger.ch>
2013-12-05 22:33:45 +0100

    Added form to add and edit items

--- Webshop/webroot/admin.php
+++ Webshop/webroot/admin.php
@@ -9,9 +9,24 @@ if(is_logged_in() && is_admin_user()) {
 	include_once(ABSPATH . "modules/functions_admin.php");
 	
 	switch($_GET["action"])  {
+		case "doedit":
+			admin_update($_GET["type"]);
+		case "doadd":
+			admin_add($_GET["type"]);
+		case "dodelete":
+			admin_delete($_GET["type"], $_POST["dodelete"]);
 		case "list":
 		default:
 			admin_list($_GET["type"]);
+			break;
+		case "edit":
+				admin_show_form($_GET["type"], $_GET["id"]);
+			break;
+		case "add":
+				admin_show_form($_GET["type"]);
+			break;
+		case "delete":
+			
 			break;
 	}
 } else {
--- Webshop/webroot/modules/functions_admin.php
+++ Webshop/webroot/modules/functions_admin.php
@@ -17,7 +17,7 @@ function admin_list($type=null) {
 function admin_list_print_type($typename) {
 	global $shopdb;
 	
-	// Dummy query to get all rows
+	// Query to get all attributes and all rows
 	$all_items = $shopdb->get_results("SELECT * FROM $shopdb->escape($typename)");
 	$attributes = $shopdb->get_col_info("name");
 		
@@ -38,6 +38,66 @@ function admin_list_print_type($typename) {
 	}
 		
 	echo "</table>";
+	echo '<a href="' . get_href("admin", array("action" => "add", "type" => $typename)) . '">[+] Add new ' . $typename . '</a>';
+	echo "<br/>";
+}
+
+function admin_show_form($type=null, $id=null) {
+	if(isset($type)) {
+		
+		global $shopdb;
+		
+		// Dummy query to only get table metadata
+		$dummy = $shopdb->get_results("SELECT * FROM $shopdb->escape($type) WHERE 0=1");
+		$attributes = $shopdb->get_col_info("name");
+		
+		if($id != null) {
+			
+			// Edit mode
+			
+			// Try to evaluate which is the ID of the type
+			foreach ( $attributes as $attribute_name ) {
+				if(preg_match("/.*\_id$/", $attribute_name) == 1) {
+					$id_attribute = $shopdb->escape($attribute_name);
+					// Break on first match
+					break;
+				}
+			}
+			
+			$edit_values = $shopdb->get_row("SELECT * FROM $shopdb->escape($type) WHERE $id_attribute = $shopdb->escape($id)");
+		}
+
+		if(isset($edit_values)) {
+			echo '<form action="' . get_href("admin", array("action" => "doedit")) . '" method="post">';
+		} else {
+			echo '<form action="' . get_href("admin", array("action" => "doadd")) . '" method="post">';
+		}
+		
+		foreach ( $attributes as $attribute_name ) {
+			// Display <input>
+			echo $attribute_name . ': <input type="text" name="';
+			echo $attribute_name . '" value="' . $edit_values->$attribute_name;
+			echo '" /><br/>';
+		}
+		
+		echo "</form>";
+		
+	} else {
+		echo "Error: No type provided";
+	}
+}
+
+function admin_add($post_array) {
+	// When adding, remove ID
+	
+	// Try to evaluate which is the ID of the type
+	foreach ( $attributes as $attribute_name ) {
+		if(preg_match("/.*\_id$/", $attribute_name) == 1) {
+			$id_attribute = $shopdb->escape($attribute_name);
+			// Break on first match
+			break;
+		}
+	}
 }
 
 ?>
