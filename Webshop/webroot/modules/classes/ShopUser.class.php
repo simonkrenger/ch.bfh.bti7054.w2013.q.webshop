c73c19f61f9eb209194c6d133acb33f75a4ed48a
Simon Krenger <simon@krenger.ch>
2013-12-17 21:47:47 +0100

    Refactored ShopUser

--- Webshop/webroot/modules/classes/ShopUser.class.php
+++ Webshop/webroot/modules/classes/ShopUser.class.php
@@ -1,10 +1,6 @@
 <?php
 class ShopUser {
 	
-	public $address;
-	public $username;
-	
-	
 	// Variables are bound dynamically
 	
 	public function ShopUser($user_id) {
@@ -21,19 +17,19 @@ class ShopUser {
 			}
 		}
 		
-		// Get address
+		// Get address (object is an attribute)
 		$this->address = new ShopUserAddress($this->address_id);
 	}
 }
 
 class ShopUserAddress {
-	public function ShopUserAddress($user_id) {
+	public function ShopUserAddress($address_id) {
 		global $shopdb;
-		
-		$clean_id = $shopdb->escape($user_id);
+
+		$clean_id = $shopdb->escape($address_id);
 		$query = sprintf("SELECT a.address_id, a.street, a.zipcode, a.city, a.country, p.name, g.name FROM address a JOIN planet p ON a.planet_id = p.planet_id JOIN galaxy g ON a.galaxy_id = g.galaxy_id WHERE address_id=%s LIMIT 1", $clean_id);
 		$user_attrs = $shopdb->get_row($query);
-		
+
 		if($user_attrs != NULL) {
 			// Dynamically bind all variables
 			foreach(get_object_vars($user_attrs) as $attr_name => $attr_value) {
@@ -42,4 +38,5 @@ class ShopUserAddress {
 		}
 	}
 }
+
 ?>
