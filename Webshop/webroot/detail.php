2ba1e4af4dd1f188b6e993d62e55b75cd4ee0802
Simon Krenger <simon@krenger.ch>
2014-01-01 16:18:04 +0100

    Reviewed task from Fränzi
    - Updated worklog (Brainstorming-ToDo)
    - Re-added line (works as expected, try with a product that has "custom attributes"

--- Webshop/webroot/detail.php
+++ Webshop/webroot/detail.php
@@ -70,8 +70,8 @@ if (isset ( $_GET ['product_id'] )) {
                         <a id="addtocartlink" href="<?php
                                 $suffix = array("product_id" => $product_id, "action" => "add");
                                 
-                                // Add custom attributes values //TODO: seems not to work yet.
-                                //$suffix = array_merge($suffix, $custom_attrs_suffix);
+                                // Add custom attributes values
+                                $suffix = array_merge($suffix, $custom_attrs_suffix);
                                 
                                 echo get_href("shoppingcart", $suffix); ?>">Add to shopping Cart</a><br>
                                                 		
